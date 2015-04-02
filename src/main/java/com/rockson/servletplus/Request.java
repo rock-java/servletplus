6c1c25a93a42d23f26d097f7ff10e5cdf39661f9
RocksonZeta <RocksonZeta@gmail.com>
2015-04-02 18:38:51 +0800

    first commit

--- src/main/java/com/rockson/servletplus/Request.java
+++ src/main/java/com/rockson/servletplus/Request.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequestWrapper;
 public class Request extends HttpServletRequestWrapper {
 	private static final Map<String, String> errors = new LinkedHashMap<String, String>();
 	protected Map<String, String> pathParams;
-	
+
 	public Request(HttpServletRequest request) {
 		super(request);
 	}
@@ -37,6 +37,39 @@ public class Request extends HttpServletRequestWrapper {
 	public void setPathParams(Map<String, String> pathParams) {
 		this.pathParams = pathParams;
 	}
-	
+
+	/**
+	 * The remote IP address of the request.
+	 * 
+	 * @param trustProxy
+	 *            If the trustProxy is setting enabled, it is the upstream address;
+	 * @return the ip of the remote host
+	 */
+	public String ip(boolean trustProxy) {
+		if (!trustProxy) {
+			return getRemoteAddr();
+		} else {
+			String[] ips = ips();
+			if (null == ips || 0 == ips.length) {
+				return getRemoteAddr();
+			} else {
+				return ips[0];
+			}
+		}
+	}
+
+	/**
+	 * this property contains an array of IP addresses specified in the “X-Forwarded-For” request header. Otherwise, it
+	 * contains an empty array.
+	 * 
+	 * For example, if “X-Forwarded-For” is “client, proxy1, proxy2”, req.ips would be ["client", "proxy1", "proxy2"],
+	 * where “proxy2” is the furthest downstream.
+	 * 
+	 * @return ips
+	 */
+	public String[] ips() {
+		String forward = getHeader("X-Forwarded-For");
+		return null == forward ? null : forward.split(",");
+	}
 
 }
--- src/main/java/com/rockson/servletplus/Response.java
+++ src/main/java/com/rockson/servletplus/Response.java
@@ -1,6 +1,12 @@
 package com.rockson.servletplus;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
@@ -12,15 +18,53 @@ public class Response extends HttpServletResponseWrapper {
 	public Response(HttpServletResponse response) {
 		super(response);
 	}
-	
-	public void json(Object object ) throws IOException {
+
+	public void json(Object object) throws IOException {
 		getWriter().write(JSON.toJSONString(object));
 	}
-	
+
+	protected void copy(InputStream in, OutputStream out, int bufferSize) throws IOException {
+		byte[] buff = new byte[bufferSize <= 0 ? 4 * 1024 : bufferSize];
+		int len;
+		while (-1 == (len = in.read(buff))) {
+			out.write(buff, 0, len);
+		}
+	}
+
 	public void send(String str) throws IOException {
-		if(null==str){
+		if (null == str) {
 			return;
 		}
 		getWriter().write(str);
+	}
+
+	public void sendFile(String str) throws IOException {
+		sendFile(FileSystems.getDefault().getPath(str));
+	}
+
+	public void sendFile(InputStream in) throws IOException {
+		copy(in, getOutputStream(), 0);
+	}
+
+	public void sendFile(Path path) throws IOException {
+		reset();
+		File file = path.toFile();
+		addHeader("Content-Length", "" + file.length());
+		addHeader("Content-Type", Files.probeContentType(path));
+		Files.copy(path, getOutputStream());
+	}
+
+	public void download(Path path) throws IOException {
+		download(path, null);
+	}
+
+	public void download(Path path, String name) throws IOException {
+		reset();
+		File file = path.toFile();
+		addHeader("Content-Disposition", "attachment; filename=" + (null == name ? file.getName() : name));
+		addHeader("Content-Length", "" + file.length());
+		addHeader("Content-Type", Files.probeContentType(path));
+		
+		Files.copy(path, getOutputStream());
 	}
 }
